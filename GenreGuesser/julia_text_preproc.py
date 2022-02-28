0d10149d23975648046f9d2de9b4c494fee4ad58
Julia Welchering <julia.welchering@gmail.com>
2022-02-28 16:05:54 +0100

    initialised clean_text function in vs code file

---
+++ GenreGuesser/julia_text_preproc.py
@@ -0,0 +1,49 @@
+import re
+import string
+string.punctuation
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+def clean_text(text):
+
+    #remove headers like [Chorus] etc
+    headers = re.findall(r"\[(.*?)\]", text)
+    for header in headers:
+        text = text.replace(header, ' ')
+
+    #separate lower/upper case words (like 'needHow')
+    text = re.findall('[A-Z][^A-Z]*', text)
+    text = " ".join(text)
+
+    #remove punctuation
+    exclude = string.punctuation + "’”“"
+    for punctuation in exclude:
+           text = text.replace(punctuation, '')
+
+    #turn text into lowercase
+    text = text.lower()
+
+    #remove numericals
+    text = ''.join(word for word in text if not word.isdigit())
+
+    #remove stopwords
+    stop_words = set(stopwords.words('english'))
+
+    #tokenise
+    word_tokens = word_tokenize(text)
+    text = [w for w in word_tokens if not w in stop_words]
+
+    #lemmatise
+    lemmatizer = WordNetLemmatizer()
+    lemmatized = [lemmatizer.lemmatize(word) for word in text]
+    text = lemmatized
+
+    text = ' '.join(text)
+
+    #rejoin "wan na" to "wanna"
+    wannas = re.findall(r"wan na", text)
+    for wanna in wannas:
+        text = text.replace(wanna, "wanna")
+
+    return text
