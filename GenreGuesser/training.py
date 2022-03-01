2cf8bffb15df1578956295096040400c9ebc01c2
Robert Pasternak <pasternakrs@gmail.com>
2022-03-01 16:39:41 +0100

    More work on the hub program (training.py)

--- GenreGuesser/training.py
+++ GenreGuesser/training.py
@@ -11,25 +11,31 @@ from GenreGuesser.data_cleaning import clean_data
 
 from sklearn.pipeline import make_pipeline
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-GENRE_DICT = {
-        '100' : 'rap',
-        '73' : 'pop',
-        '38' : 'country',
-        '114' : 'rock',
-        '57' : 'folk',
-        '62' : 'jazz'
-        }
+
+def data_prep_combined(X, y):
+    GENRE_DICT = {
+            '100' : 'rap',
+            '73' : 'pop',
+            '38' : 'country',
+            '114' : 'rock',
+            '57' : 'folk',
+            '62' : 'jazz'
+            }
+    y_out = y.apply(lambda x : GENRE_DICT[x] if x in GENRE_DICT.keys() else x)
+    X_out = X.apply(clean_text)
+    return X_out, y_out
 
 if __name__ == '__main__':
     data = pd.read_csv('raw_data/data_mini.csv')
+    print(data['Genre'].unique())
     data = clean_data(data)
-    X = data[['Lyrics']]
+    X = data['Lyrics']
     y = data['Genre']
-    y_final = y.apply(lambda x : GENRE_DICT[x] if x in GENRE_DICT.keys() else x)
-    X_final = X.copy()
-    X_final['Lyrics'] = X_final['Lyrics'].apply(clean_text)
-    pipe = make_pipeline(CountVectorizer(), KNeighborsClassifier())
-    X_final = pipe.fit_transform(X_final, y_final)
+    X_final, y_final = data_prep_combined(X, y)
+    pipe = make_pipeline(TfidfVectorizer(), KNeighborsClassifier())
+    pipe.fit(X_final, y_final)
+    vecto = TfidfVectorizer()
+    X_tests = pd.DataFrame(data = vecto.fit_transform(X_final).toarray())
     joblib.dump(pipe, 'model.joblib')
