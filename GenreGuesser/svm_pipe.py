7cccc06ec44ea3c3a27426b76e644d848ac6e537
marc <marc@marcs-mbp.localdomain>
2022-03-02 15:21:23 +0100

    create svm pipeline

---
+++ GenreGuesser/svm_pipe.py
@@ -0,0 +1,26 @@
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import svm
+from GenreGuesser.text_preproc import clean_text
+
+def format_func(X_in):
+    '''
+    Transformer function that cleans text,
+    integrated as first step of pipeline
+    '''
+    X_out = X_in.apply(clean_text)
+    return X_out
+
+# Create format_transform for data cleaning
+format_transform = FunctionTransformer(format_func)
+
+# Create Pipeline, which has the following three steps:
+#   - Clean text (remove things like '[VERSE 1]', lemmatize, etc.)
+#   - TF-IDF Vectorize
+#   - k-nearest neighbors using 5 neighbors, with weighting by distance
+svm_pipe = Pipeline([
+    ('format_transform', format_transform),
+    ('tfidf', TfidfVectorizer()),
+    ('svm', svm.SVC()),
+])
